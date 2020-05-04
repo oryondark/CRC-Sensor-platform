455ac4e37637993b57b4cccf0acf9c4a648f0532
김현준 <k2i2m2@gimhyeonjun-ui-MacBook-Pro.local>
2020-05-04 16:19:29 +0900

    model update

--- MachineLearning_example/motion_detection.ino
+++ MachineLearning_example/motion_detection.ino
@@ -145,7 +145,7 @@ void loop() {
     // restart
     count = 0;
 
-    // You must deallocate memory because Arduino not easy operate some of the garbege collection.
+    // You must deallocate memory because Arduino not easy operate some of the garbage collection.
     features = nullptr;
     delete [] data;
     data = new float[504];
