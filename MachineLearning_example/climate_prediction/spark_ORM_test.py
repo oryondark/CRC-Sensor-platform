f899563e60c18e069aa8a7900a5a7cca96addc90
김현준 <k2i2m2@gimhyeonjun-ui-MacBook-Pro.local>
2020-05-11 14:45:17 +0900

    change to ORM using Spark

--- MachineLearning_example/climate_prediction/spark_ORM_test.py
+++ MachineLearning_example/climate_prediction/spark_ORM_test.py
@@ -4,7 +4,13 @@ In this example, the ORM the sparksession read a qeury from json.
 from pyspark.sql import SparkSession
 
 #Initate SparkSession and Context
-spark = SparkSession.builder.appName('arduino-climate-rdd').getOrCreate()
+conf = SparkConf().setAppName("crc-arduino-temp-rdd").setMaster("local[*]").setAll([('spark.executor.memory', '4g'),
+                         ('spark.app.name', 'arduino-climate-rdd'),
+                         ('spark.executor.cores', '4'),
+                         ('spark.cores.max', '4'),
+                         ('spark.driver.memory','4g'),
+                         ('spark.sql.inMemoryColumnarStorage.compressed', 'true')])
+spark = SparkSession.builder.config(conf=conf).getOrCreate()
 sc = spark.sparkContext
 
 def showing_schema(weather_json):
@@ -34,4 +40,3 @@ def convert_to_datetime(weather_json):
     dt_test = weather_df.select(weather_df['current']['dt']).collect()[0]['current.dt']
     real_time = datetime.fromtimestamp(int(dt_test)).strftime('%Y-%m-%d %H:%M:%S')
     print(real_time)
-    
